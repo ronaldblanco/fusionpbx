5ab5a525667c1e4eeb1f71bf255cfd429f4aa535
samael33 <IgorOlhovskiy@gmail.com>
2017-07-13 17:20:01 +0300

    Dial default gw

--- resources/install/scripts/app/custom/dial_default_gateway/index.lua
+++ resources/install/scripts/app/custom/dial_default_gateway/index.lua
@@ -1,4 +1,6 @@
---connect to the database
+-- Select a default gateway for domain and choose it for outbound dialing
+
+-- connect to the database
 require "resources.functions.database_handle"
 
 local dbh = database_handle('system');
@@ -10,7 +12,6 @@ if (session:ready()) then
 
     if (domain_id == nil) then
         domain_id = session:getVariable("domain_name") 
-    end
     else
         sql = "SELECT gateway_uuid FROM v_gateways "
         sql = sql .. "WHERE domain_uuid = '"..domain_id.."' "
@@ -33,27 +34,12 @@ if (session:ready()) then
             freeswitch.consoleLog("NOTICE", "[dial_default_gateway] Dialing through gateway "..gateway_uuid.."\n");
             local callee_id_number = session:getVariable("callee_id_number")
             callee_id_number = callee_id_number and callee_id_number or "" 
-            session:execute("bridge", "sofia/gateway" .. gateway_uuid .. "/" .. callee_id_number)
+            session:execute("bridge", "sofia/gateway/" .. gateway_uuid .. "/" .. callee_id_number)
         else
             freeswitch.consoleLog("NOTICE", "[dial_default_gateway] Cannot get gateway for domain\n");
         end
-        -- session:setVariable("default_gateway", default_gateway)
     else
         freeswitch.consoleLog("NOTICE", "[dial_default_gateway] Cannot get domain_uuid or domain_id\n");
         session:execute('info')
     end
 end
-
--- sql = "SELECT * FROM v_ring_groups ";
--- sql = sql .. "where ring_group_uuid = '"..ring_group_uuid.."' ";
--- status = dbh:query(sql, function(row)
---         domain_uuid = row["domain_uuid"];
---         ring_group_name = row["ring_group_name"];
---         ring_group_extension = row["ring_group_extension"];
---         ring_group_forward_enabled = row["ring_group_forward_enabled"];
---         ring_group_forward_destination = row["ring_group_forward_destination"];
---         ring_group_cid_name_prefix = row["ring_group_cid_name_prefix"];
---         ring_group_cid_number_prefix = row["ring_group_cid_number_prefix"];
---         missed_call_app = row["ring_group_missed_call_app"];
---         missed_call_data = row["ring_group_missed_call_data"];
--- end);
