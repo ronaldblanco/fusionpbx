e4f199220c12a7c668491fd0ef2703fbdeb92fdf
samael33 <IgorOlhovskiy@gmail.com>
2017-08-16 12:07:07 +0200

    Reviews and ongoing work

--- app/destinations/app_config.php
+++ app/destinations/app_config.php
@@ -159,5 +159,7 @@
 		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
 		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "Enter the accountcode.";
 		$z++;
-
+		$apps[$x]['db'][$y]['fields'][$z]['name'] = "destination_description";
+		$apps[$x]['db'][$y]['fields'][$z]['type'] = "text";
+		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = "Enter the description.";
 ?>
