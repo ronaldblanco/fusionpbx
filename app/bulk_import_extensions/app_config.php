d14d700a35f23a393b368bf9227d7604ec485772
samael33 <IgorOlhovskiy@gmail.com>
2018-07-12 14:40:30 +0300

    Add prototype

---
+++ app/bulk_import_extensions/app_config.php
@@ -0,0 +1,29 @@
+<?php
+
+	//application details
+		$apps[$x]['name'] = "Import Extensions";
+		$apps[$x]['uuid'] = "9257cf72-e70c-4187-a84b-cefe66a9ebeb";
+		$apps[$x]['category'] = "Switch";
+		$apps[$x]['subcategory'] = "";
+		$apps[$x]['version'] = "";
+		$apps[$x]['license'] = "Mozilla Public License 1.1";
+		$apps[$x]['url'] = "http://www.fusionpbx.com";
+		$apps[$x]['description']['en-us'] = "Possibility to import extensions and devices from CSV file";
+		$apps[$x]['description']['es-cl'] = "";
+		$apps[$x]['description']['de-de'] = "";
+		$apps[$x]['description']['de-ch'] = "";
+		$apps[$x]['description']['de-at'] = "";
+		$apps[$x]['description']['fr-fr'] = "";
+		$apps[$x]['description']['fr-ca'] = "";
+		$apps[$x]['description']['fr-ch'] = "";
+		$apps[$x]['description']['pt-pt'] = "";
+		$apps[$x]['description']['pt-br'] = "";
+
+	//permission details
+		$y = 0;
+		$apps[$x]['permissions'][$y]['name'] = "import_extensions";
+		$apps[$x]['permissions'][$y]['menu']['uuid'] = "70934a0a-b49f-4e91-ba11-a7849cc30ed6";
+		$apps[$x]['permissions'][$y]['groups'][] = "superadmin";
+		$apps[$x]['permissions'][$y]['groups'][] = "admin";
+		$y++;
+?>
