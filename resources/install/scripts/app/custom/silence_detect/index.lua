e3e563f065a40ba3b4262e5a97113e882a511d81
samael33 <IgorOlhovskiy@gmail.com>
2018-11-13 12:12:48 +0200

    Add advanced mode to silence_detect

--- resources/install/scripts/app/custom/silence_detect/index.lua
+++ resources/install/scripts/app/custom/silence_detect/index.lua
@@ -3,9 +3,14 @@ require "app.custom.silence_detect.resources.functions.wav"
 
 opthelp = [[
  -a, --algo=OPTARG                  Algorythm used. lines or samples
+ -m, --mode=OPTARG                  Could be simple or advanced
  -s, --temporary-storage=OPTARG     Temporary storage
- -l, --loops=OPTARG                 Loop count
- -r, --ringback=OPTARG              Ringback to be used
+ -l, --loops=OPTARG                 Loop count on simple mode. On advanced mode applies to initial loop before fake an answer
+ -p, --post-loops=OPTARG            Loop count on after answer on advanced mode.
+ -q, --silenece-duration=OPTARG     Silence duration on post-answer in advanced mode. Miliseconds
+ -r, --ringback=OPTARG              Ringback to be used in simple mode and first part in advanced
+ -f, --hello-file=OPTARG             File to be used on answer. Imitate 'hello'. streamfile.lua is used to play this
+ -g, --ignore-silence-detect        Ignore silence detect results on first part of advanced mode
  -t, --transfer-on-silence=OPTARG   Where to transfer on silence
  -h, --hangup-cause=OPTARG          Set hangup cause if transfer-on-silence is hangup
  -i, --include-pattern=COUNT        Include callerid_number pattern     
@@ -102,6 +107,7 @@ if session:ready() then
     local transfer_on_silence = opts.t or 'hangup'
     local ringback = opts.r or session:getVariable('ringback') or "%(2000,4000,440,480)"
     local tmp_dir = opts.s or '/dev/shm/'
+    local mode = opts.m or 'simple'
     -- Prepare args table to hold algo options only
     table.remove(args, 1)
 
@@ -121,6 +127,7 @@ if session:ready() then
     -- Answer the call
     session:answer()
 
+    -- For both simple and advanced mode first part is same
     for i = 1, loop_count do
         if opts.v then
             freeswitch.consoleLog("NOTICE", "[silence_detect] Loop:" .. i .. ', algorithm is ' .. algo .. ' ' .. table.concat(args, " "))
@@ -137,7 +144,7 @@ if session:ready() then
             silence_detect_debug_info = "Channel hung up"
         end
         if opts.d then
-            session:execute("export", "silence_detect_" .. algo .. "_" .. i .. "=" .. silence_detect_debug_info)
+            session:execute("export", "silence_detect_" .. mode .. "_" .. algo .. "_" .. i .. "=" .. silence_detect_debug_info)
         end
         if opts.k then
             os.rename(tmp_file_name, tmp_file_name .. "_loop_" .. i)
@@ -147,6 +154,46 @@ if session:ready() then
         if (is_silence_detected == false) then
             loop_detected = i
             break
+        end
+    end
+
+    -- If mode is advanced and silence detected on previius steps or we ignore all silence before - proceed to next step
+    if (mode == 'advanced' and (is_silence_detected or opts.g)) then
+        local hello_file = opts.f or ''
+        local post_loop_count = opts.p or 5
+        local silence_duration = opts.q or 1000
+
+        -- Playbach hello here
+        session:execute('lua', 'streamfile.lua '.. hello_file)
+
+        for i = loop_count, loop_count + post_loop_count do
+            if opts.v then
+                freeswitch.consoleLog("NOTICE", "[silence_detect] Loop:" .. i .. ', algorithm is ' .. algo .. ' ' .. table.concat(args, " "))
+            end
+            session:execute("record_session", tmp_file_name)
+            session:execute("playback", 'silence_stream://' .. silence_duration)
+            session:execute("stop_record_session", tmp_file_name)
+
+            if session:ready() then
+                is_silence_detected, silence_detect_debug_info = silence_detect_file(tmp_file_name, algo, args)
+            else
+                is_silence_detected = false
+                silence_detect_debug_info = "Channel hung up"
+            end
+            -- Debug part
+            if opts.d then
+                session:execute("export", "silence_detect_" .. mode .. "_" .. algo .. "_" .. i .. "=" .. silence_detect_debug_info)
+            end
+            -- Keep recorded file
+            if opts.k then
+                os.rename(tmp_file_name, tmp_file_name .. "_loop_" .. i)
+            else
+                os.remove(tmp_file_name)
+            end
+            if (is_silence_detected == false) then
+                loop_detected = i
+                break
+            end
         end
     end
 
