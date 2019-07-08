2d2059164b7b3ef0f817eaccbd384b288d2e5bc4
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2019-07-08 15:05:58 +0300

    Adding extension_queue function on 1-key exit

--- resources/install/scripts/extension_queue.lua
+++ resources/install/scripts/extension_queue.lua
@@ -24,23 +24,21 @@
 --	Riccardo Granchi <riccardo.granchi@nems.it>
 
 --include config.lua
-	require "resources.functions.config";
+	require "resources.functions.config"
 
 if (session:ready()) then
-	fifo_simo = session:getVariable("fifo_simo");
-		if (not fifo_simo) then fifo_simo = '1'; end
-	fifo_timeout = session:getVariable("fifo_timeout");
-		if (not fifo_timeout) then fifo_timeout = '10'; end
-	fifo_lag = session:getVariable("fifo_lag");
-		if (not fifo_lag) then fifo_lag = '10'; end
+	fifo_simo = session:getVariable("fifo_simo") or '1'
+	fifo_timeout = session:getVariable("fifo_timeout") or '10'
+	fifo_lag = session:getVariable("fifo_lag") or '10'
+	fifo_exit_key = session:getVariable("fifo_exit_key")
 
 	extension_queue = session:getVariable("extension_queue");
-	extension = string.sub(extension_queue, string.len("queue_") + 1 );
+	extension = string.sub(extension_queue, string.len("queue_") + 1 )
 
 	-- freeswitch.consoleLog("notice", "Extension Queue [" .. extension_queue .. "]\n");
 
-	api = freeswitch.API();
-	fifo_count = api:executeString("fifo count " .. extension_queue);
+	api = freeswitch.API()
+	fifo_count = api:executeString("fifo count " .. extension_queue)
 
 	-- freeswitch.consoleLog("notice", "fifo count " .. fifo_count .. "]\n");
 
@@ -48,24 +46,31 @@ if (session:ready()) then
 	i = 0;
 	v = {};
 	for w in string.gmatch(fifo_count,"[^:]+") do
-		v[i] = w;
-		i = i + 1;
+		v[i] = w
+		i = i + 1
 	end
 
-	fifo_name = v[0];
-	consumer_count = v[1];
-	caller_count = v[2];
-	member_count = v[3];
-	ring_consumer_count = v[4];
-	idle_consumer_count = v[5];
+	fifo_name = v[0]
+	consumer_count = v[1]
+	caller_count = v[2]
+	member_count = v[3]
+	ring_consumer_count = v[4]
+	idle_consumer_count = v[5]
 
 	if( not (member_count == "0") ) then
-		freeswitch.consoleLog("notice", "Adding member [" .. extension .. "] to fifo " .. extension_queue .. " \n");
+		freeswitch.consoleLog("notice", "Adding member [" .. extension .. "] to fifo " .. extension_queue .. " \n")
 
-		session:execute("set", "fifo_member_add_result=${fifo_member(add " .. extension_queue .." {fifo_member_wait=nowait}user/" .. extension .. " " ..fifo_simo .. " " ..fifo_timeout .. " " .. fifo_lag .. "} )"); --simo timeout lag
+		session:execute("set", "fifo_member_add_result=${fifo_member(add " .. extension_queue .." {fifo_member_wait=nowait}user/" .. extension .. " " ..fifo_simo .. " " ..fifo_timeout .. " " .. fifo_lag .. "} )") --simo timeout lag
 	end;
 
-	-- Answerinf the call
-	session:answer();
-	session:execute( "fifo", extension_queue .. " in" );
+	-- Answering the call
+	if (fifo_exit_key) then
+		fifo_exit_destination = session:getVariable("fifo_exit_destination") or "*99" .. session:getVariable("$1")
+		context = session:getVariable("context") or session:getVariable("domain_name")
+		
+		session:execute("bind_digit_action", extension_queue .. "," .. fifo_exit_key .. ",exec:transfer," .. fifo_exit_destination .. " XML " .. context)
+		session:execute("digit_action_set_realm", extension_queue)
+	end
+	session:answer()
+	session:execute( "fifo", extension_queue .. " in" )
 end
