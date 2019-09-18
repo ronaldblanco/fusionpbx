e984c2b2de287ceb8231787b9b6f6d3431164dfb
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2019-09-18 17:04:02 +0300

    ...

--- app/sms/resources/classes/check_acl.php
+++ app/sms/resources/classes/check_acl.php
@@ -3,123 +3,58 @@
 if (!class_exists('check_acl')) {
 	class check_acl {
 
-		private static $_IP_TYPE_SINGLE = 'single';
-		private static $_IP_TYPE_WILDCARD = 'wildcard';
-		private static $_IP_TYPE_MASK = 'mask';
-		private static $_IP_TYPE_CIDR = 'CIDR';
-		private static $_IP_TYPE_SECTION = 'section';
+		private $allow_nodes;
 
-		private $db;
+		public function __construct($db, $domain_name) {
 
-		public function __construct($db) {
-			$this->db = $db;
+			// Example of output
+			//default_policy	node_policy		node_cidr
+			//deny				deny			1.1.1.1/32
+			//deny				allow			0.0.0.0/0
 
-			$sql = "SELECT node_cidr FROM v_access_control_nodes";
-			$sql .= " WHERE node_cidr != ''";
-			$sql .= " AND node_type = 'allow'";
-			$sql .= " ";
+			$sql =  "SELECT v_access_controls.access_control_default as default_policy";
+			$sql .= " v_access_control_nodes.node_type as node_policy";
+			$sql .= " v_access_control_nodes.node_cidr as node_cidr ";
+			$sql .= "FROM v_access_control_nodes JOIN v_access_controls";
+			$sql .= " ON v_access_controls.access_control_uuid = v_access_control_nodes.access_control_uuid ";
+			$sql .= "WHERE v_access_controls.access_control_name = 'sms'";
+			$sql .= " AND v_access_control_nodes.node_domain = '" . $domain_name . "'";
+			$sql .= " ORDER BY node_policy DESC";
+
 		}
 
-		public function check($ip, $allowed_ips) {
+		function check_acl_1() {
+			global $db, $debug, $domain_uuid, $domain_name;
 
-			foreach ($allowed_ips as $allowed_ip) {
-				$type = $this->_judge_ip_type($allowed_ip);
-				$sub_rst = call_user_func(array($this, '_sub_checker_' . $type), $allowed_ip, $ip);
-
-				if ($sub_rst) {
-					return true;
-				}
+			//select node_cidr from v_access_control_nodes where node_cidr != '';
+			$sql = "select node_cidr from v_access_control_nodes where node_cidr != '' and node_type = 'allow'";
+			$prep_statement = $db->prepare(check_sql($sql));
+			$prep_statement->execute();
+			$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
+			if (count($result) == 0) {
+				die("No ACL's");
+			}
+			foreach ($result as &$row) {
+				$allowed_ips[] = $row['node_cidr'];
 			}
 
-			return false;
+			$acl = new IP4Filter($allowed_ips);
+
+			return $acl->check($_SERVER['REMOTE_ADDR'], $allowed_ips);
 		}
 
-
-
-		private function _judge_ip_type($ip) {
-			if (strpos($ip, '*')) {
-				return self :: $_IP_TYPE_WILDCARD;
-			}
-
-			if (strpos($ip, '/')) {
-				$tmp = explode('/', $ip);
-				if (strpos($tmp[1], '.')) {
-					return self :: $_IP_TYPE_MASK;
-				} else {
-					return self :: $_IP_TYPE_CIDR;
-				}
-			}
-
-			if (strpos($ip, '-')) {
-				return self :: $_IP_TYPE_SECTION;
-			}
-
-			if (ip2long($ip)) {
-				return self :: $_IP_TYPE_SINGLE;
-			}
-
-			return false;
+		function ipCIDRCheck ($IP, $CIDR) {
+			list ($net, $mask) = split ("/", $CIDR);
+		
+			$ip_net = ip2long ($net);
+			$ip_mask = ~((1 << (32 - $mask)) - 1);
+		
+			$ip_ip = ip2long ($IP);
+		
+			$ip_ip_net = $ip_ip & $ip_mask;
+		
+			return ($ip_ip_net == $ip_net);
 		}
-
-		private function _sub_checker_single($allowed_ip, $ip) {
-			return (ip2long($allowed_ip) == ip2long($ip));
-		}
-
-		private function _sub_checker_wildcard($allowed_ip, $ip) {
-			$allowed_ip_arr = explode('.', $allowed_ip);
-			$ip_arr = explode('.', $ip);
-			for ($i = 0; $i < count($allowed_ip_arr); $i++) {
-				if ($allowed_ip_arr[$i] == '*') {
-					return true;
-				} else {
-					if (false == ($allowed_ip_arr[$i] == $ip_arr[$i])) {
-						return false;
-					}
-				}
-			}
-		}
-
-		private function _sub_checker_mask($allowed_ip, $ip) {
-			list($allowed_ip_ip, $allowed_ip_mask) = explode('/', $allowed_ip);
-			$begin = (ip2long($allowed_ip_ip) & ip2long($allowed_ip_mask)) + 1;
-			$end = (ip2long($allowed_ip_ip) | (~ ip2long($allowed_ip_mask))) + 1;
-			$ip = ip2long($ip);
-			return ($ip >= $begin && $ip <= $end);
-		}
-
-		private function _sub_checker_section($allowed_ip, $ip) {
-			list($begin, $end) = explode('-', $allowed_ip);
-			$begin = ip2long($begin);
-			$end = ip2long($end);
-			$ip = ip2long($ip);
-			return ($ip >= $begin && $ip <= $end);
-		}
-
-		private function _sub_checker_CIDR($CIDR, $IP) {
-			list ($net, $mask) = explode('/', $CIDR);
-			return ( ip2long($IP) & ~((1 << (32 - $mask)) - 1) ) == ip2long($net);
-		}
-
-	}
-
-	function check_acl_1() {
-		global $db, $debug, $domain_uuid, $domain_name;
-
-		//select node_cidr from v_access_control_nodes where node_cidr != '';
-		$sql = "select node_cidr from v_access_control_nodes where node_cidr != '' and node_type = 'allow'";
-		$prep_statement = $db->prepare(check_sql($sql));
-		$prep_statement->execute();
-		$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
-		if (count($result) == 0) {
-			die("No ACL's");
-		}
-		foreach ($result as &$row) {
-			$allowed_ips[] = $row['node_cidr'];
-		}
-
-		$acl = new IP4Filter($allowed_ips);
-
-		return $acl->check($_SERVER['REMOTE_ADDR'],$allowed_ips);
 	}
 }
 ?>
