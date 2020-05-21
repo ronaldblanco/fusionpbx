f0028b6cc5b961e6102365fd04385a9ce2f4bb1e
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-05-21 11:53:03 +0300

    MOH_FIX rework and remove of prepare_callerid

--- resources/install/scripts/app/custom/moh_fix/index.lua
+++ resources/install/scripts/app/custom/moh_fix/index.lua
@@ -1,25 +1,63 @@
 -- modify MOH from local_stream to file_stream
 require "app.custom.moh_fix.resources.functions.local_to_file_stream"
 
+function fsLog(message)
+    if opts.v then
+        freeswitch.consoleLog("NOTICE", "[MOH FIX] " .. message .. "\n")
+    end
+end
+
+
+opthelp = [[
+ -s, --sort-mode                    Sort Mode. As of now only Random is supported. By default - no sort
+ -v, --verbose                      If specified - verbose info is printed on FS console
+ -t, --transfer-ringback=OPTARG     True/False. Default - True. Set also transfer ringback
+ -r, --ringback=OPTARG              True/False. Default - True. Set also transfer ringback
+]]
+
+
+opts, args, err = require('app.custom.functions.optargs').from_opthelp(opthelp, argv)
+
+if opts == nil then
+    freeswitch.consoleLog("ERROR", "[MOH FIX] Options are not parsable " .. err)
+    do return end
+end
+
 if (session:ready()) then
+
+    -- Check already fixed
+    local moh_fix = session:getVariable("moh_fix")
+    if (moh_fix and moh_fix == 'true') then
+        fsLog("MOH is fixed already")
+        do return end
+    end
+
     hold_music = session:getVariable("hold_music")
-    --freeswitch.consoleLog("notice", "[FIX_MOH] Got moh "..hold_music.."\n")
+    
     if (hold_music ~= nil) then
-        hold_music = local_to_file_stream(hold_music)
+        hold_music = local_to_file_stream(hold_music, opts.s)
         session:execute("export", "hold_music=" .. hold_music)
     end
+    fsLog("hold_music set")
 
-    transfer_ringback = session:getVariable("transfer_ringback")
-    if (transfer_ringback ~= nil) then
-        transfer_ringback = local_to_file_stream(transfer_ringback)
-        session:execute("export", "transfer_ringback=" .. transfer_ringback)
+    if (opts.t == nil or opts.t == 'true') then
+        transfer_ringback = session:getVariable("transfer_ringback")
+        if (transfer_ringback ~= nil) then
+            transfer_ringback = local_to_file_stream(transfer_ringback, opts.s)
+            session:execute("export", "transfer_ringback=" .. transfer_ringback)
+        end
+        fsLog("transfer_ringback set")
     end
 
-    ringback = session:getVariable("ringback")
-    if (ringback ~= nil) then
-        ringback = local_to_file_stream(ringback)
-        session:execute("export", "ringback=" .. ringback)
+    if (opts.r == nil or opts.r == 'true') then
+        ringback = session:getVariable("ringback")
+        if (ringback ~= nil) then
+            ringback = local_to_file_stream(ringback, opts.s)
+            session:execute("export", "ringback=" .. ringback)
+        end
+        fsLog("ringback set")
     end
-    --freeswitch.consoleLog("notice", "[FIX_MOH] Set moh "..hold_music.."\n")
-    session:setVariable("fix_moh", "true")
+
+    -- Prevent double call of script
+    session:setVariable("moh_fix", "true")
 end
--- resources/install/scripts/app/custom/moh_fix/resources/functions/local_to_file_stream.lua
+++ resources/install/scripts/app/custom/moh_fix/resources/functions/local_to_file_stream.lua
@@ -1,9 +1,9 @@
-function local_to_file_stream(stream)
+function local_to_file_stream(stream, is_random)
 
     if (string.find(stream, "local_stream://", 1, true)) then
         local api = freeswitch.API()
 
-        freeswitch.consoleLog("NOTICE", "[local_to_file_stream] Got this stream - "..stream.."\n");
+        fsLog("Got stream "..stream);
         local local_stream_list = api:executeString("local_stream show") -- Getting all classes. As a bonus - file path's
         local current_stream = stream:gsub("local_stream://", "")
         local found_stream = ""
@@ -14,22 +14,29 @@ function local_to_file_stream(stream)
                 break
             end
         end
-        freeswitch.consoleLog("NOTICE", "[local_to_file_stream] found this - "..found_stream.."\n");
+        fsLog("Found this "..found_stream);
 
         if (found_stream ~= "") then
-            local class, location = found_stream:match("([^,]+),([^,]+)") -- Get path to folder
-            local files = io.popen('find "'..location..'" -type f | awk \'/wav/ || /mp3/\'') -- Get file list
+            local _, location = found_stream:match("([^,]+),([^,]+)") -- Get path to folder
+            local find_command = 'find "' .. location .. '" -type f | awk \'/wav/ || /mp3/\''
+            if (is_random ~= nil) then
+                fsLog("Sorting in random order")
+                find_command = find_command .. " | sort -R --random-source=/dev/urandom"
+            end
+            local files = io.popen(find_command) -- Get file list
             local files_string = ""
 
             for file in files:lines() do -- Generating file_string:// string
                 files_string = files_string..file.."!"
             end
 
-        files_string_orig = files_string
-        while (files_string_orig..files_string):len() < 4000 do
-            files_string = files_string..files_string_orig
-        end
+            files_string_orig = files_string
 
+            while (files_string_orig..files_string):len() < 4000 do
+                files_string = files_string..files_string_orig
+            end
+
+            -- Remove last "!"
             if (string.len(files_string) ~= 0) then
                 files_string = "file_string://"..files_string:sub(1, #files_string - 1)
                 return files_string
