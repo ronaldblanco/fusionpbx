76631f50f7d57ff1922851579e8b9af3db2de4e0
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-05-26 18:29:17 +0300

    crm_call_1 callback added

---
+++ app/xml_cdr/resources/classes/crm_call_1.php
@@ -0,0 +1,105 @@
+<?php
+
+if (!class_exists('crm_call_1')) {
+	class crm_call_1 {
+
+        public $is_ready;
+
+        public function __construct($session = False) {
+
+            $this->is_ready = True;
+        }
+
+        # Just a failsafe not to throw an error
+        public function __call($name, $arguments) {
+            return;
+        }
+
+
+        public function process(&$xml_varibles) {
+
+            $url =  strlen($xml_varibles->crm_end_settings_url) > 0 ? base64_decode(urldecode($xml_varibles->crm_end_settings_url), true) : False;
+
+            if (!$url or strlen($url) == 0) {
+                return;
+            }
+
+            $send_data = array(
+                'url' => $url,
+                'fields' => array(
+                    'timestamp' => $xml_varibles->end_epoch,
+                    'direction' => $xml_varibles->direction,
+                    'ivr_path' => $xml_varibles->ivr_path,
+                    'uuid' => $xml_varibles->uuid,
+                )
+            );
+            // Get correct hangup
+            switch ($xml_varibles->hangup_cause) {
+                case 'NORMAL_CLEARING':
+                    $send_data['fields']['status'] = 'answered';
+                    break;
+                case 'CALL_REJECTED':
+                case 'SUBSCRIBER_ABSENT':
+                case 'USER_BUSY':
+                    $send_data['fields']['status'] = 'busy';
+                    break;
+                case 'NO_ANSWER':
+                case 'NO_USER_RESPONSE':
+                case 'ORIGINATOR_CANCEL':
+                case 'LOSE_RACE': // This cause usually in ring groups, so this call is not ended.
+                    $send_data['fields']['status'] = 'no answer';
+                    break;
+                default:
+                    $send_data['fields']['status'] = 'failed';
+            }
+
+            $send_data['fields']['src'] = array(
+                'name' => $xml_varibles->caller_id_name,
+                'number' => $xml_varibles->caller_id_number,
+            );
+
+            $send_data['fields']['last_seen'] = array(
+                'name' => strlen(strval($xml_varibles->last_sent_callee_id_name)) > 0 ? strval($xml_varibles->last_sent_callee_id_name) : strval($xml_varibles->caller_destination),
+                'number' => strlen(strval($xml_varibles->last_sent_callee_id_number)) > 0 ? strval($xml_varibles->last_sent_callee_id_number) : strval($xml_varibles->caller_destination)
+            );
+
+            $send_data['fields']['time'] = array(
+                'duration' => $xml_varibles->duration,
+                'answered' => $xml_varibles->billsec,
+            );
+
+            if (strlen(strval($xml_varibles->vtiger_record_path)) > 0) {
+                $send_data['fields']['recording'] = base64_decode(urldecode($xml_varibles->vtiger_record_path));
+            }
+
+            $this->send($send_data);
+        }
+
+        private function send($data) {
+
+            
+            $data_string = json_encode($data['fields']);
+
+            $ch = curl_init($this->url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+            curl_setopt($ch, CURLOPT_HEADER, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json',
+                                                    'Content-Length: ' . strlen($data_string)
+                                                ));
+
+            $resp = curl_exec($ch);
+            curl_close($ch);
+
+            file_put_contents('/tmp/api_crm_call_1.log', " -> ".$data['url']. " Req:".$data_string." Resp:".$resp."\n");
+
+        }
+    }
+}
+
+
+?>
