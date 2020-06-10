67bcc1339e1cf881abdaa798d31c9e19fcd53f91
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-06-10 18:34:13 +0300

    Fix field name

--- app/xml_cdr/resources/classes/vtiger_connector.php
+++ app/xml_cdr/resources/classes/vtiger_connector.php
@@ -32,7 +32,7 @@ if (!class_exists('vtiger_connector')) {
                     'direction' => strval($xml_varibles->direction),
                     'vtigersignature' => $key,
                     'uuid' => strval($xml_varibles->uuid),
-                    'callstate' => 'call_end',
+                    'callstatus' => 'call_end',
                 )
             );
             // Get correct hangup
